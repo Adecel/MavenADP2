4011ee4af3e1f66f3ba5337df5da7fc8222c262d
untobeko <216108055@mycput.ac.za>
2021-04-01 22:36:12 +0200

    my first commit in the branch

---
+++ src/main/java/za/ac/cput/assignmentmaven/miniCulculator.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.assignmentmaven;
+
+/**
+ *
+ * @author Noku
+ */
+public class miniCulculator {
+       /**
+     * 
+     * program to add and subtract numbers
+     * 
+     */
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+    
+    public int mutltyPly(int a, int b)
+    {
+        return a*b;
+    }
+          
+      
+    
+    public String printCulName( String name){
+       
+           return name;
+    }
+    
+    
+}
