a5e9f67bb8de4558083d3458deb2a749646e1224
ChanteLewis <chantelewis97@gmail.com>
2021-04-08 19:29:24 +0200

    My commit from my local branch.

---
+++ src/main/java/za/ac/cput/assignmentmaven/Lists.java
@@ -0,0 +1,40 @@
+package za.ac.cput.assignmentmaven;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ChanteLewis_216118395
+ */
+public class Lists {
+         private List list = new ArrayList();
+
+	public boolean testList(){
+            return list.isEmpty();
+        }
+   
+   
+        public void testAddElements(String name) {
+		list.add(0, name);
+	}
+
+        public void testSetElementNull(){
+            list.set(0, null);
+        }
+        
+        public void testSetElement(){
+            list.set(0, "name");
+        }
+	
+        public void testRemoveElement(String name){
+            list.remove(name);
+        }
+}
---
+++ src/test/java/za/ac/cput/assignmentmaven/ListsTest.java
@@ -0,0 +1,118 @@
+package za.ac.cput.assignmentmaven;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+/**
+ *
+ * @author ChanteLewis_216118395
+ */
+public class ListsTest {
+
+	private List list;
+	
+	@BeforeEach
+	public void setUp(){
+		list = new ArrayList();
+	}
+	
+	@Test
+	public void testList(){
+		assertTrue(list.isEmpty());
+		assertTrue(list.size() == 0);
+	}
+	
+	
+	@Test
+	public void testAddElements(){
+		list.add(0, "Leanie");
+		list.add(1, "Rema");
+		list.add(2, "Denver");
+		
+		assertEquals("Leanie", list.get(0));
+		assertEquals("Rema", list.get(1));
+		assertEquals("Denver", list.get(2));
+		
+		list.add(3, "Chante");
+                
+                assertEquals("Leanie", list.get(0));
+		assertEquals("Rema", list.get(1));
+		assertEquals("Denver", list.get(2));
+		assertEquals("Chante", list.get(3));	
+		
+		assertTrue(list.size()==4);
+	}
+	
+	
+        //Failing Test
+	@Test 
+	public void testSetElementNull(){
+		list.add(0, "Khayla");
+		list.set(0, null);
+                fail();
+	}
+	
+	@Test
+	public void testSetElement(){
+		list.add(0, "Leanie");
+		list.add(1, "Rema");
+		list.add(2, "Denver");
+                
+		list.set(1, "Jakes");
+		
+		assertEquals("Leanie", list.get(0));
+		assertEquals("Jakes", list.get(1));
+		assertEquals("Denver", list.get(2));
+	}
+	
+	@Test
+	public void testRemoveElement(){
+                list.add(0, "Leanie");
+		list.add(1, "Rema");
+		list.add(2, "Denver");
+		assertEquals("Denver", list.remove(2));
+		assertTrue(list.size() == 2);
+	}
+        
+        @Test 
+	public void testCompare(){
+                list.add(0, "Leanie");
+		list.add(1, "Rema");
+		list.add(2, "Denver");
+                list.add(3,"Leanie");
+		assertSame(list.get(0), list.get(3));
+	}
+        
+        //Disabled Test
+        @Disabled("testRemoveEmptyList is disabled")
+	@Test 
+	public void testRemoveEmptyList(){
+		list.remove(0);
+	}
+        
+        @Test
+        @Timeout(value = 5000, unit = TimeUnit.MILLISECONDS)
+        public void testTimeout(){
+	}
+    
+	}
+
+
+
+
